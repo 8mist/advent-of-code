289a83d16e8fefa0e108d5259767135a1fe6f5a9
Grégoire Ciles <gregoire@staays.com>
2024-12-08 21:56:31 +0100

    chore: format code

--- 2024/day-03/part_2.ts
+++ 2024/day-03/part_2.ts
@@ -10,13 +10,16 @@ const dontDoRegex = /don\'t\(\).*?do\(\)/gs;
 const mulRegex = /mul\((\d{1,3}),\s*(\d{1,3})\)/g;
 
 const format = (input: string): RegExpExecArray[] => {
-	return Array.from(input.replace(/\r/g, "").trim().replace(dontDoRegex, "").matchAll(mulRegex));
+	return Array.from(
+		input.replace(/\r/g, "").trim().replace(dontDoRegex, "").matchAll(mulRegex),
+	);
 };
 
 export const solve = (input: string) => {
 	const matches = format(input);
 	return matches.reduce(
-		(sum, match) => sum + Number.parseInt(match[1], 10) * Number.parseInt(match[2], 10),
+		(sum, match) =>
+			sum + Number.parseInt(match[1], 10) * Number.parseInt(match[2], 10),
 		0,
 	);
 };
