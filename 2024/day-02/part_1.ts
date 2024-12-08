8ca0a41cc098b84b7b94a5ddea291d2e37e055e5
Grégoire Ciles <gregoire@staays.com>
2024-12-08 21:22:23 +0100

    feat(day02): add part 1

---
+++ 2024/day-02/part_1.ts
@@ -0,0 +1,43 @@
+import fs from "node:fs";
+
+const testInput = fs.readFileSync(
+	new URL("./input.test", import.meta.url),
+	"utf-8",
+);
+export const testCase = [testInput, 2];
+
+const format = (input: string): Array<Array<number>> => {
+	return input
+		.replace(/\r/g, "")
+		.trim()
+		.split("\n")
+		.map((line) => line.trim().split(/\s+/).map(Number));
+};
+
+const isSafeReport = (report: Array<number>): boolean => {
+	let isSafe = true;
+	let isIncreasing: boolean | undefined = undefined;
+
+	for (let i = 0; i < report.length - 1; i++) {
+		const delta = report[i + 1] - report[i];
+
+		if (Math.abs(delta) > 3 || delta === 0) {
+			isSafe = false;
+			break;
+		}
+
+		if (isIncreasing === undefined) {
+			isIncreasing = delta > 0;
+		} else if ((isIncreasing && delta < 0) || (!isIncreasing && delta > 0)) {
+			isSafe = false;
+			break;
+		}
+	}
+
+	return isSafe;
+};
+
+export const solve = (input: string) => {
+	const reports = format(input);
+	return reports.filter(isSafeReport).length;
+};
