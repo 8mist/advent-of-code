ec3011ddbcfbd8957aebda05d57b9a85f86285a7
Grégoire Ciles <gregoire@staays.com>
2024-12-08 20:58:24 +0100

    feat: add day-01

---
+++ 2024/day-01/part_1.ts
@@ -0,0 +1,25 @@
+import fs from 'node:fs';
+
+const testInput = fs.readFileSync(new URL('./input.test', import.meta.url), 'utf-8');
+export const testCase = [testInput, 11];
+
+const format = (input: string): [number[], number[]] => {
+  const lines = input.replace(/\r/g, '').trim().split('\n')
+  const left: number[] = [];
+  const right: number[] = [];
+
+  for (const line of lines) {
+    const [before, after] = line.split(/\s+/).map(Number);
+    left.push(before);
+    right.push(after);
+  }
+
+  return [left, right];
+};
+
+export const solve = (input: string) => {
+  const [left, right] = format(input);
+  return left
+    .sort((a, b) => a - b)
+    .reduce((total, value, index) => total + Math.abs(value - right.sort((a, b) => a - b)[index]), 0);
+};
---
+++ 2024/day-01/part_2.ts
@@ -0,0 +1,29 @@
+import fs from 'node:fs';
+
+const testInput = fs.readFileSync(new URL('./input.test', import.meta.url), 'utf-8');
+export const testCase = [testInput, 31];
+
+const format = (input: string): [number[], number[]] => {
+  const lines = input.replace(/\r/g, '').trim().split('\n')
+  const left: number[] = [];
+  const right: number[] = [];
+
+  for (const line of lines) {
+    const [before, after] = line.split(/\s+/).map(Number);
+    left.push(before);
+    right.push(after);
+  }
+
+  return [left, right];
+};
+
+export const solve = (input: string) => {
+  const [left, right] = format(input);
+
+  const rightCounts = right.reduce<Record<number, number>>((counts, num) => {
+    counts[num] = (counts[num] || 0) + 1;
+    return counts;
+  }, {});
+
+  return left.reduce((total, num) => total + (rightCounts[num] || 0) * num, 0);
+};
