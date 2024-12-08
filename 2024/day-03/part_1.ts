b277a12c8c08345dcf55b651642a47809b58c700
Grégoire Ciles <gregoire@staays.com>
2024-12-08 21:43:38 +0100

    feat(day03): add part 1

---
+++ 2024/day-03/part_1.ts
@@ -0,0 +1,22 @@
+import fs from "node:fs";
+
+const testInput = fs.readFileSync(
+	new URL("./input.test", import.meta.url),
+	"utf-8",
+);
+export const testCase = [testInput, 161];
+
+const regex = /mul\((\d{1,3}),\s*(\d{1,3})\)/g;
+
+const format = (input: string): RegExpExecArray[] => {
+	return Array.from(input.replace(/\r/g, "").trim().matchAll(regex));
+};
+
+export const solve = (input: string) => {
+	const matches = format(input);
+	return matches.reduce(
+		(sum, match) =>
+			sum + Number.parseInt(match[1], 10) * Number.parseInt(match[2], 10),
+		0,
+	);
+};
