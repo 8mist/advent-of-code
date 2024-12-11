246484012f4ff28374375f3a9cf929666d0a37ff
Grégoire Ciles <gregoire@staays.com>
2024-12-11 06:49:27 +0100

    perf: optimize day-11

--- 2024/day-11/part_1.ts
+++ 2024/day-11/part_1.ts
@@ -6,36 +6,39 @@ const testInput = fs.readFileSync(
 );
 export const testCase = [testInput, 55312];
 
-const format = (input: string): string[] => {
-	return input.replace(/\r/g, "").trim().split(" ");
+const format = (input: string): number[] => {
+	return input.replace(/\r/g, "").trim().split(" ").map(Number);
 };
 
-const applyTransformation = (stones: string[]): string[] => {
-  const newStones: string[] = [];
-  for (const stone of stones) {
-    if (stone === "0") {
-      newStones.push("1");
-    } else if (stone.length % 2 === 0) {
-      const mid = stone.length / 2;
-      const left = stone.slice(0, mid);
-      const right = stone.slice(mid).replace(/^0+/, "");
-      if (left) newStones.push(left);
-      if (right) {
-        newStones.push(right);
-      } else {
-        newStones.push("0");
-      }
-    } else {
-      newStones.push((parseInt(stone) * 2024).toString());
-    }
-  }
-  return newStones;
+const memoizationCache = new Map<string, number>();
+
+const transformStones = (stoneValue: number, steps: number) => {
+	const key = `${stoneValue},${steps}`;
+	if (memoizationCache.has(key)) {
+		return memoizationCache.get(key)!;
+	}
+
+	let result: number;
+	if (steps === 0) {
+		result = 1;
+	} else if (stoneValue === 0) {
+		result = transformStones(1, steps - 1);
+	} else if (stoneValue.toString().length % 2 === 0) {
+		const dstr = stoneValue.toString();
+		const mid = Math.floor(dstr.length / 2);
+		const left = parseInt(dstr.slice(0, mid), 10);
+		const right = parseInt(dstr.slice(mid), 10);
+		result =
+			transformStones(left, steps - 1) + transformStones(right, steps - 1);
+	} else {
+		result = transformStones(stoneValue * 2024, steps - 1);
+	}
+
+	memoizationCache.set(key, result);
+	return result;
 };
 
 export const solve = (input: string) => {
-  let stones = format(input);
-  for (let i = 0; i < 25; i++) {
-    stones = applyTransformation(stones);
-  }
-  return stones.length;
+	let stones = format(input);
+	return stones.reduce((sum, x) => sum + transformStones(x, 25), 0);
 };
