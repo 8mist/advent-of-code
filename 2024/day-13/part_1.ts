012fda48a4dbdf55382a5625518b2ea2e56198a2
Grégoire Ciles <gregoire@staays.com>
2024-12-15 10:55:01 +0100

    feat: add Day 13 Claw Contraption

---
+++ 2024/day-13/part_1.ts
@@ -0,0 +1,31 @@
+import fs from "node:fs";
+
+const testInput = fs.readFileSync(
+	new URL("./input.test", import.meta.url),
+	"utf-8",
+);
+export const testCase = [testInput, 480];
+
+const format = (input: string): number[][] => {
+	return input
+		.replace(/\r/g, "")
+		.trim()
+		.split("\n\n")
+		.map((machine: string) => machine.match(/\d+/g)!.map(Number));
+};
+
+export const solve = (input: string) => {
+	const machines = format(input);
+	let sum = 0;
+	for (const [dx1, dy1, dx2, dy2, x, y] of machines) {
+		const a = (x * dy2 - y * dx2) / (dx1 * dy2 - dx2 * dy1);
+		if (Number.isInteger(a)) {
+			const xRemain = x - dx1 * a;
+			const b = xRemain / dx2;
+			if (Number.isInteger(b) && dy1 * a + dy2 * b === y) {
+				sum += a * 3 + b;
+			}
+		}
+	}
+	return sum;
+};
