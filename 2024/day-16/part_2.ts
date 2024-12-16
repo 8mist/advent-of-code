01ce90bea3efb25a36999b31946849e9daec3613
Grégoire Ciles <gregoire@staays.com>
2024-12-16 16:39:06 +0100

    feat: add Day 16 Reindeer Maze

---
+++ 2024/day-16/part_2.ts
@@ -0,0 +1,97 @@
+import fs from "node:fs";
+
+const testInput = fs.readFileSync(
+	new URL("./input.test", import.meta.url),
+	"utf-8",
+);
+export const testCase = [testInput, 45];
+
+const format = (input: string): string[] => {
+	return input.replace(/\r/g, "").trim().split("\n");
+};
+
+type Direction = [number, number];
+type Path = [number, number][];
+
+interface QueueItem {
+	i: number;
+	j: number;
+	dirIdx: number;
+	score: number;
+	path: Path;
+}
+
+const DIRECTIONS: Direction[] = [
+	[0, 1],
+	[1, 0],
+	[0, -1],
+	[-1, 0],
+];
+
+export const solve = (input: string) => {
+	const lines = format(input);
+
+	let start: [number, number] = [-1, -1];
+
+	const map = lines.map((line, i) =>
+		line.split("").map((char, j) => {
+			if (char === "S") start = [i, j];
+			return char;
+		}),
+	);
+
+	let queue: QueueItem[] = [
+		{ i: start[0], j: start[1], dirIdx: 0, score: 0, path: [start] },
+	];
+
+	const paths: Record<number, Path[]> = {};
+	const seen = map.map((row) =>
+		row.map(() => DIRECTIONS.map(() => Number.MAX_SAFE_INTEGER)),
+	);
+	let minScore = Number.MAX_SAFE_INTEGER;
+
+	while (queue.length > 0) {
+		const nextQueue: QueueItem[] = [];
+
+		for (const { i, j, dirIdx, score, path } of queue) {
+			if (score > minScore) continue;
+
+			if (map[i][j] === "E") {
+				minScore = Math.min(minScore, score);
+				if (score === minScore) {
+					paths[minScore] = paths[minScore] || [];
+					paths[minScore].push(path);
+				}
+				continue;
+			}
+
+			if (seen[i][j][dirIdx] < score) continue;
+			seen[i][j][dirIdx] = score;
+
+			for (let dirIdx2 = 0; dirIdx2 < 4; dirIdx2++) {
+				if (dirIdx2 === (dirIdx + 2) % 4) continue;
+
+				const [di, dj] = DIRECTIONS[dirIdx2];
+				const i2 = i + di;
+				const j2 = j + dj;
+
+				if (".E".includes(map[i2]?.[j2])) {
+					nextQueue.push({
+						i: i2,
+						j: j2,
+						dirIdx: dirIdx2,
+						score: score + 1 + (dirIdx2 !== dirIdx ? 1000 : 0),
+						path: [...path, [i2, j2]],
+					});
+				}
+			}
+		}
+
+		queue = nextQueue;
+	}
+
+	const uniqueTiles = new Set(
+		paths[minScore].flat().map((coords) => coords.join(",")),
+	);
+	return uniqueTiles.size;
+};
